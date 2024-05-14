ab9027db77de86b03a4f6a4b2a89c2cb44dcbfb4
riham <riham.bouchiha@usmba.ac.ma>
2024-05-14 17:05:17 +0100

    commit15

--- authentification/js/mainInscription.js
+++ authentification/js/mainInscription.js
@@ -1,4 +1,4 @@
 document.getElementById("loginLink").addEventListener("click", function(event) {
-    event.preventDefault(); // Empêche le comportement par défaut du lien
-    window.location.href = "indexSeconnecter.html"; // Redirige vers la page indexSeconnecter.html
+    event.preventDefault(); 
+    window.location.href = "indexSeconnecter.html";
 });
--- authentification/js/mainSeconnecter.js
+++ authentification/js/mainSeconnecter.js
@@ -1,36 +1,8 @@
-document.getElementById("inscriptionLink").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    window.location.href = "indexInscription.html"; 
-});
-                        
-
-  
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
-        try {
-            const response = await fetch('/seconnecter', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ adresse_mail: email, mot_de_passe: password })
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                console.log('Données enregistrées avec succès :', data);
-            } else {
-                console.error('Erreur lors de l\'enregistrement des données');
-            }
-        } catch (error) {
-            console.error('Erreur lors de la requête :', error);
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll("a[href='indexSeconnecter.html']").forEach(function(element) {
+        element.addEventListener("click", function(event) {
+            event.preventDefault(); 
+            window.location.href = "indexSeconnecter.html"; 
+        });
     });
 });
